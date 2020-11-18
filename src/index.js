574b12ac576e04856c20a52ec84738d3b869f6e7
MaksymStaskiv <maksymstaskiv@gmail.com>
2020-11-18 14:35:11 +0200

    'update'

--- src/index.js
+++ src/index.js
@@ -4,13 +4,28 @@ import './index.css';
 //import Player from 'react-player'
 import reportWebVitals from './reportWebVitals';
 import ResponsivePlayer from './video/ResponsivePlayer';
- 
-
+import Navbar from './components/navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Catalog from './pages/catalog';
+import Main from './pages/main';
+import SignUp from './pages/signup';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+       
+        <Route path='/about' component={About} />
+        <Route path='/catalog' component={Catalog} />
+        <Route path='/main' component={Main} />
+        <Route path='/sign-up' component={SignUp} />
+      </Switch>
+    </Router>
     
-    <ResponsivePlayer />,
 
   </React.StrictMode>,
   document.getElementById('root')
--- src/video/ResponsivePlayer.jsx
+++ src/video/ResponsivePlayer.jsx
@@ -7,18 +7,19 @@ const ResponsivePlayer = () => {
 
     return (
      
-        <div className='player-wrapper'>
+        /*<div className='player-wrapper'>*/
           <ReactPlayer
             //ref={this.ref}
             url='https://projectvideobacket.s3.amazonaws.com/testvideo%20%281%29.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVK5MRPWLEKRQ3MUX%2F20201112%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20201112T133304Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=33839ea0371910df80a8fb3cd0485344086dc44cadc9270763ab181cbd637151'
             className='react-player'
             playing
             controls
-            width='75%'
-            height='75%'
+            width='50%'
+            height='55%'
+
             //onProgress={this.handleProgress}
           />
-        </div>
+       // </div>
      
      
     )
