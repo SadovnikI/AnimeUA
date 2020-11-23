88e4a84da1d39642b06bc94fcbfcac0a2c3ae5cb
IlliaKadobanskij <kadobanskij@gmail.com>
2020-11-23 13:16:12 +0200

    Postgres + Js + model changings

--- Website_project/main_page/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Category, Genre, Movie, Bucket, Video, Serial
-
-admin.site.register(Category)
-admin.site.register(Genre)
-admin.site.register(Movie)
-admin.site.register(Bucket)
-admin.site.register(Video)
-admin.site.register(Serial)
---
+++ backend/src/movies/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import MovieListView, MovieDetailView
+
+urlpatterns = [
+    path('', MovieListView.as_view()),
+    path('<pk>', MovieDetailView.as_view()),
+]
