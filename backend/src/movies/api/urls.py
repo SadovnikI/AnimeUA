880317c6865f6170afe1d23be6e0cfafe3597ff7
IlliaKadobanskij <kadobanskij@gmail.com>
2020-11-24 15:40:47 +0200

    deleted

--- backend/src/movies/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import MovieListView, MovieDetailView
-
-urlpatterns = [
-    path('', MovieListView.as_view()),
-    path('<pk>', MovieDetailView.as_view()),
-]
