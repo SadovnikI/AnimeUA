a011c5ef69000119b59dfccaff59fb8fb3924c4d
IlliaKadobanskij <kadobanskij@gmail.com>
2020-11-26 14:45:58 +0200

    Try

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,30 +1,29 @@
-import React, { Component } from "react";
-import RoomJoinPage from "./RoomJoinPage";
+import React, {Component} from "react";
 import CreateRoomPage from "./CreateRoomPage";
+
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
 } from "react-router-dom";
 
 export default class HomePage extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <p>This is the home page</p>
-          </Route>
-          <Route path="/join" component={RoomJoinPage} />
-          <Route path="/create" component={CreateRoomPage} />
-        </Switch>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <p>POSOSU</p>
+                    </Route>
+                    <Route path="/create" component={CreateRoomPage}/>
+                </Switch>
+            </Router>
+        );
+    }
 }
